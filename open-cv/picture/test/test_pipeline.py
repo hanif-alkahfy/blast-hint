7a0d45849e988c227ee375450955b26683dae55c
hanif-alkahfy <hanifalkahfy@gmail.com>
2025-07-17 16:21:47 +0700

    first commit

--- open-cv/picture/test/test_pipeline.py
+++ open-cv/picture/test/test_pipeline.py
@@ -6,6 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from extractors.pipeline import extract_game_state
 from vis.visualizer import visualize_grid_and_blocks
 
-img = cv2.imread('./assets/test_image_11.png')
+img = cv2.imread('./assets/test_image_10.png')
 game_state = extract_game_state(img)
 visualize_grid_and_blocks(game_state["grid"], game_state["blocks"])
